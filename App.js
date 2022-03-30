f2f5fa3a4abb2346aeb9d95acbb89c39b9b16886
Erwan-k <kerbrat.erwan@gmail.com>
2022-03-30 16:25:53 +0200

    function -> const

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 import PagePrincipale from "./components/PagePrincipale.js";
 
-function App = () =>{
+const App = () =>{
 	return(
 		<Router>
 			<Routes>
